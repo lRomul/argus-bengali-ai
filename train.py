49f5cd11e884ea6a08d4c6a07b869f0455482f38
ruslan <ruslan1123@gmail.com>
2020-01-26 14:07:07 +0300

    feat: Train experiment sm_seresnext50_001

--- train.py
+++ train.py
@@ -33,9 +33,10 @@ DEVICES = ['cuda']
 
 SAVE_DIR = config.experiments_dir / args.experiment
 PARAMS = {
-    'nn_module': ('CustomResnet', {
-        'encoder': 'resnet50',
-        'pretrained': True
+    'nn_module': ('cnn_finetune', {
+        'model_name': 'se_resnext50_32x4d',
+        'pretrained': True,
+        'dropout_p': 0.0
     }),
     'loss': ('BengaliAiCrossEntropy', {
         'grapheme_weight': 9.032258064516129 * 2,
