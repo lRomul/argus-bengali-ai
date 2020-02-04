5dc2d651636e64608b9bc551632bc7b0b765fd9f
ruslan <ruslan1123@gmail.com>
2020-02-04 21:10:05 +0300

    feat: Train experiment stages_001

--- train.py
+++ train.py
@@ -25,9 +25,9 @@ parser.add_argument('--experiment', required=True, type=str)
 parser.add_argument('--fold', required=False, type=int)
 args = parser.parse_args()
 
-IMAGE_SIZE = [128, 176, 224, 256]
-BATCH_SIZE = [512, 256, 176, 144]
-TRAIN_EPOCHS = [50, 50, 50, 150]
+IMAGE_SIZE = [128, 176, 224]
+BATCH_SIZE = [512, 256, 176]
+TRAIN_EPOCHS = [40, 40, 120]
 BASE_LR = 0.001
 NUM_WORKERS = 8
 USE_AMP = True
@@ -90,9 +90,9 @@ def train_fold(save_dir, train_folds, val_folds):
 
         callbacks = [
             MonitorCheckpoint(save_dir, monitor='val_hierarchical_recall', max_saves=1),
-            EarlyStopping(monitor='val_hierarchical_recall', patience=40),
+            EarlyStopping(monitor='val_hierarchical_recall', patience=30),
             ReduceLROnPlateau(monitor='val_hierarchical_recall',
-                              factor=0.64, patience=9, threshold=1e-7),
+                              factor=0.64, patience=7, threshold=1e-7),
             LoggingToFile(save_dir / 'log.txt')
         ]
 
