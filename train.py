76bafe090931474e50513c164dc61f59f8543649
Ruslan Baikulov <ruslan1123@gmail.com>
2020-02-04 12:20:17 +0300

    feat: Train experiment cosann_001

--- train.py
+++ train.py
@@ -7,7 +7,7 @@ from argus.callbacks import (
     MonitorCheckpoint,
     EarlyStopping,
     LoggingToFile,
-    ReduceLROnPlateau
+    CosineAnnealingWarmRestarts
 )
 
 from torch.utils.data import DataLoader
@@ -25,9 +25,9 @@ parser.add_argument('--experiment', required=True, type=str)
 parser.add_argument('--fold', required=False, type=int)
 args = parser.parse_args()
 
-IMAGE_SIZE = [128, 176, 224, 256]
-BATCH_SIZE = [512, 256, 176, 144]
-TRAIN_EPOCHS = [50, 50, 50, 150]
+IMAGE_SIZE = [128, 224]
+BATCH_SIZE = [512, 176]
+TRAIN_EPOCHS = [80, 180]
 BASE_LR = 0.001
 NUM_WORKERS = 8
 USE_AMP = True
@@ -90,9 +90,8 @@ def train_fold(save_dir, train_folds, val_folds):
 
         callbacks = [
             MonitorCheckpoint(save_dir, monitor='val_hierarchical_recall', max_saves=1),
-            EarlyStopping(monitor='val_hierarchical_recall', patience=40),
-            ReduceLROnPlateau(monitor='val_hierarchical_recall',
-                              factor=0.64, patience=9, threshold=1e-7),
+            EarlyStopping(monitor='val_hierarchical_recall', patience=50),
+            CosineAnnealingWarmRestarts(T_0=12, T_mult=2),
             LoggingToFile(save_dir / 'log.txt')
         ]
 
