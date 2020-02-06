86873139af2891ebfa6dd8d330e73b47f9feb103
Ruslan Baikulov <ruslan1123@gmail.com>
2020-02-06 11:41:45 +0300

    feat: Train experiment stages_003

--- train.py
+++ train.py
@@ -26,7 +26,7 @@ parser.add_argument('--fold', required=False, type=int)
 args = parser.parse_args()
 
 IMAGE_SIZE = [128, 176, 224]
-BATCH_SIZE = [512, 256, 176]
+BATCH_SIZE = [320, 160, 110]
 TRAIN_EPOCHS = [40, 40, 120]
 BASE_LR = 0.001
 NUM_WORKERS = 8
@@ -42,7 +42,7 @@ def get_lr(base_lr, batch_size):
 SAVE_DIR = config.experiments_dir / args.experiment
 PARAMS = {
     'nn_module': ('CustomResnet', {
-        'encoder': 'gluon_resnet50_v1d',
+        'encoder': 'gluon_seresnext50_32x4d',
         'pretrained': True,
         'classifier': ('fc', {'pooler': 'avgpool'})
     }),
@@ -92,7 +92,7 @@ def train_fold(save_dir, train_folds, val_folds):
             MonitorCheckpoint(save_dir, monitor='val_hierarchical_recall', max_saves=1),
             EarlyStopping(monitor='val_hierarchical_recall', patience=30),
             ReduceLROnPlateau(monitor='val_hierarchical_recall',
-                              factor=0.8, patience=3, threshold=1e-7),
+                              factor=0.64, patience=7, threshold=1e-7),
             LoggingToFile(save_dir / 'log.txt')
         ]
 
