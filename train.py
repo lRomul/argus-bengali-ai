ef861e1f509c64acfa24c34839ac30aaba5187e4
ruslan <ruslan1123@gmail.com>
2020-02-22 20:12:40 +0300

    feat: Train experiment cbam_006

--- train.py
+++ train.py
@@ -56,7 +56,7 @@ PARAMS = {
         'vowel_weight': 0.5913978494623656,
         'consonant_weight': 0.3763440860215054,
         'smooth_factor': 0.1,
-        'ohem_rate': 1.0
+        'ohem_rate': 0.8
     }),
     'optimizer': ('AdamW', {'lr': get_lr(BASE_LR, BATCH_SIZE[0])}),
     'device': DEVICES[0]
