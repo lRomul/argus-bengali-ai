4fc564a90f27b5c1e4fa52fd16d413ec860c579d
ruslan <ruslan1123@gmail.com>
2020-02-27 07:47:31 +0300

    feat: Update timm in deps feat: Predict experiment skresnext50_002

--- data/bengali-ai-deps/git_clone.sh
+++ data/bengali-ai-deps/git_clone.sh
@@ -1,4 +1,4 @@
 git clone --depth 1 -b master https://github.com/Cadene/pretrained-models.pytorch
 git clone --depth 1 -b master https://github.com/creafz/pytorch-cnn-finetune
 git clone -b master https://github.com/rwightman/pytorch-image-models
-cd pytorch-image-models && git checkout 4808b3c32ffda5aa26c28e31a4b23ea6ef1969d1
+cd pytorch-image-models && git checkout cc5a11abbaddef1aa3feb29009e57ca335b9cf57
