66e1f180219c68325b762a38a976ba85f7644d69
ruslan <ruslan1123@gmail.com>
2020-03-14 03:42:57 +0300

    feat: Train experiment unseen_005

--- make_folds.py
+++ make_folds.py
@@ -9,7 +9,7 @@ from src import config
 
 if __name__ == '__main__':
     random_state = 43
-    num_unseen_graphemes = 200
+    num_unseen_graphemes = 300
 
     random.seed(random_state)
     np.random.seed(random_state)
